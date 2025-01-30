04fd40de2b45f293b18af8a798687c7a82b84a44
KramLololo <markbgmc@gmail.com>
2025-01-30 22:06:08 +0200

    Initialize stop IDs

--- burgasbus-scraper/main.cpp
+++ burgasbus-scraper/main.cpp
@@ -1,9 +1,10 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 
-#include <iostream>
-#include <string_view>
 #include <format>
+#include <string_view>
+#include <deque>
+#include <iostream>
 using namespace std::literals;
 
 int main()
@@ -13,6 +14,11 @@ int main()
 	constexpr auto routesApiUrl        = "https://telelink.city/api/v1/949021bc-c2c0-43ad-a146-20e19bbc3649/transport/planner/routes"sv;
 	constexpr auto stopId              = 340; // TODO: Remove after implementing requesting
 
+	// Initialize deque with all stop IDs TODO: Benchmark performance of different containers after implementing requesting
+	std::deque<int> stopIds;
+	for (const auto stops = nlohmann::json::parse(Get(cpr::Url{stopsApiUrl}).text);
+		 const auto &stop: stops)
+		stopIds.push_back(stop.at("id"));
 
 	const cpr::Response response = Get(cpr::Url{std::format(requestTemplate, stopId)});
 	const nlohmann::json body = nlohmann::json::parse(response.text);
