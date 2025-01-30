51d7ec5b60dc4d2d9adb1c576b83a741cc6bbcce
KramLololo <markbgmc@gmail.com>
2025-01-30 20:43:49 +0200

    Clean up variable name

--- burgasbus-scraper/main.cpp
+++ burgasbus-scraper/main.cpp
@@ -8,13 +8,13 @@ using namespace std::literals;
 
 int main()
 {
-	constexpr auto stopRequestTemplate = "https://telelink.city/api/v1/949021bc-c2c0-43ad-a146-20e19bbc3649/transport/planner/stops/{}/times"sv;
+	constexpr auto requestTemplate = "https://telelink.city/api/v1/949021bc-c2c0-43ad-a146-20e19bbc3649/transport/planner/stops/{}/times"sv;
 	constexpr auto stopsApiUrl         = cpr::Url{"https://telelink.city/api/v1/949021bc-c2c0-43ad-a146-20e19bbc3649/transport/planner/stops"sv};
 	constexpr auto routesApiUrl        = cpr::Url{"https://telelink.city/api/v1/949021bc-c2c0-43ad-a146-20e19bbc3649/transport/planner/routes"sv};
 	constexpr auto stopId              = 340; // TODO: Remove after implementing requesting
 
 
-	const cpr::Response response = Get(cpr::Url{std::format(templateUrl, station)});
+	const cpr::Response response = Get(cpr::Url{std::format(requestTemplate, stopId)});
 	const nlohmann::json body = response.text;
 
 	if (response.status_code == 200)
